5f1bc9b1b2fc197ac3ba57f937e8944029b9bf70
Tashiro Mako <Mako9675@users.noreply.github.com>
2023-01-14 11:29:44 +0000

    [finish] showのエラーが出ている

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,18 +12,24 @@ class ListsController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:id]) 
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-  end
+  # def update
+  #   book = Book.find(params[:id])
+  #   book.update(book_params)
+  #   redirect_to book_path(book.id)
+  # end
+
+  # def destroy
+  #   book = Book.find(params[:id])
+  #   book.destroy
+  #   redirect_to book_path(book.id)
+  # end
 
 private
   def book_params
