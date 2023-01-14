71e56d9b2093b8ac1426e29932ea4a35dc7ac120
Tashiro Mako <Mako9675@users.noreply.github.com>
2023-01-14 07:11:32 +0000

    finish] 投稿フォーム

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,23 @@
 class ListsController < ApplicationController
 
-  def create
+  def index
+    @book = Book.all
   end
 
-  def index
-    @lists = Book.all
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/lists/index'
   end
 
   def show
   end
 
   def edit
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:body, :title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # resorces :lists
-  get 'lists/new'
-
   get 'lists/index'
   post 'lists' => 'lists#create'
   get 'lists/show'
   get 'lists/edit'
-  get '/top'=> 'homes#top'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
