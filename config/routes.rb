005f0136c543131bab8eecdb59f39e6ef8672abf
Tashiro Mako <Mako9675@users.noreply.github.com>
2023-01-15 06:11:48 +0000

    [add] resourcesの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  # resorces :lists
+  resources :lists
   get '/top' => 'homes#top'
-  get 'lists/index'
-  post 'lists' => 'lists#create'
-  get 'lists/:id' => 'lists#show',as: 'book'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_book'
-  patch 'lists/:id' => 'lists#update', as: 'update_book'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_book'
+  # get 'lists/index'
+  # post 'lists' => 'lists#create'
+  # get 'lists/:id' => 'lists#show',as: 'book'
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_book'
+  # patch 'lists/:id' => 'lists#update', as: 'update_book'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
